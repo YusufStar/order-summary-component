1147be8095769a79662f611f164e0a9d7ab75ec3
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-10-16 22:50:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <h1 className="font-[900] desk:text-3xl mobile:2xl text-dark-blue mt-10">Order Summary</h1>
         <h1 className="text-center text-desaturated-blue font-[500] desk:text-[16px] mobile:text-[14px] mt-7">You can now listen to millions of songs,<br/>audiobooks, and podcasts on any device<br/>anywhere you like!</h1>
         <div className="w-[75%] h-[90px] bg-very-pale-blue flex flex-row items-center justify-between rounded-xl mt-5">
-          <img src={musicicon} alt="music" className="ml-5 desk:w-[48px] mobile:w-[32px]" />
+          <img src={musicicon} className="ml-5 desk:w-[48px] mobile:w-[32px]"/>
           <div className="h-[50%] w-[50%] flex flex-col items-start desk:ml-0 mobile:ml-2 justify-center">
             <h1 className="text-dark-blue font-[900] desk:text-[16px] mobile:text-[12px]">Annual Plan</h1>
             <h1 className="text-desaturated-blue font-[900] desk:text-[16px] mobile:text-[12px]">$59.99/year</h1>
           </div>
           <a href="#" className="mr-5 font-[700] desk:text-[15px] mobile:text-[11px] underline text-bright-blue hover:no-underline hover:opacity-80 transition-all duration-200">Change</a>
         </div>
-        <button className="w-[75%] h-[50px] rounded-xl bg-bright-blue shadow-xl mt-5 font-[700] text-[16px] text-white hover:opacity-80 transition-all duration-200">Proceed to Payment</button>
+        <a className="w-[75%] h-[50px] rounded-xl bg-bright-blue shadow-xl mt-5 font-[700] text-[16px] text-white hover:opacity-80 transition-all duration-200 flex items-center justify-center">Proceed to Payment</a>
         <h1 className="text-[15px] text-desaturated-blue mt-5 font-[900] hover:text-dark-blue cursor-pointer transition-all duration-200">Cancel Order</h1>
       </div>
     </div>
